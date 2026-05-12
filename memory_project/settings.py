580da9a4a8be06c261289803186d482a045ffed6
srinjay98 <Srinjay8024@gmail.com>
2026-05-12 16:22:08 +0530

    Fixed Cloudinary storage for Django 6

--- memory_project/settings.py
+++ memory_project/settings.py
@@ -189,7 +189,16 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'RVtKEzxipgmgpLgRnKHVYMT_yjw',
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # =========================================
